4a0fe4b2143dd6970b10065de49ddc285f8f3302
RomSargsyan <romsargsyan20@gmail.com>
2020-08-27 11:08:35 +0400

    Add screenshots and Fixes

--- src/components/Profile/ProfileInfo/ProfileData/ProfileData.jsx
+++ src/components/Profile/ProfileInfo/ProfileData/ProfileData.jsx
@@ -2,53 +2,54 @@ import React from 'react';
 import UserAvatar from './../../../../assets/images/UserAvatar.png';
 
 const ProfileData = ({ profile, editProfileData, isOwner, saveProfilePhoto }) => {
-  const onChangePhoto = (event) => {
-    if (event.target.files.length > 0) {
-      saveProfilePhoto(event.target.files[0])
-    }
-  }
+	const onChangePhoto = (event) => {
+		if (event.target.files.length > 0) {
+			saveProfilePhoto(event.target.files[0])
+		}
+	}
 
-  return (
-    <div>
-      <div className="edit-profile">
-        {!isOwner && <button className="btn btn-success" onClick={editProfileData}>Edit</button>}
-      </div>
-      <div className="row">
-        <div className="col-6">
-          <div className="pl-4">
-            <img
-              alt="Profile"
-              src={profile.photos.small || UserAvatar}
-            />
-            {!isOwner && <div><input type="file" onChange={onChangePhoto} /></div>}
-          </div>
-          <ul className="list-group list-group-flush pt-3">
-            <li className="list-group-item">
-              <strong>Full Name </strong> {profile.fullName}
-            </li>
-            <li className="list-group-item">
-              <strong>About me</strong> {profile.aboutMe || '-----'}
-            </li>
-            <li className="list-group-item">
-              <strong>looking For a Job</strong> {profile.lookingForAJob ? 'Yes' : 'No'}
-            </li>
-            <li className="list-group-item">
-              <strong>Job Skills</strong> {profile.lookingForAJobDescription || '----'}
-            </li>
-          </ul>
-        </div>
-        <div className="col-6">
-          <ul className="list-group list-group-flush">
-            {Object.keys(profile.contacts).map((key, index) => {
-              return <li key={key} className="list-group-item">
-                <strong>{key}</strong> {profile.contacts[key] || '-----'}
-              </li>
-            })}
-          </ul>
-        </div>
-      </div>
-    </div>
-  )
+	return (
+		<div>
+			<div className="edit-profile">
+				{!isOwner && <button className="btn btn-success" onClick={editProfileData}>Edit</button>}
+			</div>
+			<div className="row">
+				<div className="col-6">
+					<div className="pl-4">
+						<img
+							alt="profile"
+							src={profile.photos.small || UserAvatar}
+							className="rounded-circle p-2 user-avatar"
+						/>
+						{!isOwner && <div><input type="file" onChange={onChangePhoto} /></div>}
+					</div>
+					<ul className="list-group list-group-flush pt-3">
+						<li className="list-group-item">
+							<strong>Full Name </strong> {profile.fullName}
+						</li>
+						<li className="list-group-item">
+							<strong>About me</strong> {profile.aboutMe || '-----'}
+						</li>
+						<li className="list-group-item">
+							<strong>looking For a Job</strong> {profile.lookingForAJob ? 'Yes' : 'No'}
+						</li>
+						<li className="list-group-item">
+							<strong>Job Skills</strong> {profile.lookingForAJobDescription || '----'}
+						</li>
+					</ul>
+				</div>
+				<div className="col-6">
+					<ul className="list-group list-group-flush">
+						{Object.keys(profile.contacts).map((key, index) => {
+							return <li key={key} className="list-group-item">
+								<strong>{key}</strong> {profile.contacts[key] || '-----'}
+							</li>
+						})}
+					</ul>
+				</div>
+			</div>
+		</div>
+	)
 }
 
 export default ProfileData;
--- src/components/Users/User.jsx
+++ src/components/Users/User.jsx
@@ -3,28 +3,32 @@ import { NavLink } from "react-router-dom";
 import userAvatar from "./../../assets/images/UserAvatar.png";
 
 const User = ({ user, isFollowing, onFollow, onUnfollow }) => {
+	console.log(user);
 	return (
 		<tbody>
 			<tr>
 				<td>
-					<div className="row">
-						<NavLink to={`/profile/${user.id}`} className="nav-link col-1">
-							<img
-								className="rounded-circle image-profile"
-								alt="User avatar"
-								src={user.photos.small || user.photos.large || userAvatar}
-							/>
-						</NavLink>
-						<div className="col-11 pr-5">{user.name}</div>
-					</div>
+					<NavLink to={`/profile/${user.id}`} className="nav-link">
+						<div className="row align-items-center">
+							<div className="col-1">
+								<img
+									className="rounded-circle image-profile"
+									alt="User avatar"
+									src={user.photos.small || user.photos.large || userAvatar}
+								/>
+							</div>
+							<div className="col-5">{user.name}</div>
+						</div>
+					</NavLink>
 				</td>
 				<td>{user.status}</td>
 				<td>Armenia</td>
 				<td>Yerevan</td>
 				<td>
-					{user.followed ? (
+					{!user.followed ? (
+						
 						<button
-							className="btn btn-success text-whithe"
+							className="btn btn-primary text-whithe"
 							disabled={isFollowing.some(id => id === user.id)}
 							onClick={() => {
 								onFollow(user.id);
